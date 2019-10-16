97b6dc83887c812ce940e38d2c57e3f9587c7abb
vinogradov artem <vinogradov44@outlook.com>
2019-10-16 14:51:22 +0300

    добавил 2 компонента

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  arr = [1, 1, 2, 3, 5, 8, 13];
-
-  obj = [
-    {
-      title: 'First title',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, harum.'
-    },
-    {
-      title: 'Second title',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, officiis?'
-    }
-  ];
-
   constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
 import {FormsModule} from '@angular/forms';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    PostFormComponent,
   ],
   imports: [
     BrowserModule,
